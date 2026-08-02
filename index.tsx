af682cf9bcd9117549c4fa1150a0b175509b2406
kalmsong <158127619+kalmsong@users.noreply.github.com>
2026-08-02 14:31:25 +0900

    Load the compiled application stylesheet from Vite

--- index.tsx
+++ index.tsx
@@ -6,6 +6,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AuthBootstrap from './AuthBootstrap';
+import './index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
